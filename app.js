ab8178d36b4cfa7a725510de845e8e61f8fd7e23
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-12-31 14:37:14 +0400

    feat:middleware for unknow endpoint and error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 
 mongoose
   .connect(MONGODB_URI)
@@ -14,5 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
