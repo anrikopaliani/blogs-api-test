2ebe15ec3d86adbf835d26cc0fbcf2a3ddde2f6a
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-12-31 14:00:49 +0400

    feat: blogs get and post request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
 
 mongoose
   .connect(MONGODB_URI)
@@ -11,5 +12,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,1 +1,23 @@
 const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
+
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+    author: body.author,
+  });
+
+  const savedBlog = await blog.save();
+
+  response.status(201).json(savedBlog);
+});
+
+module.exports = blogsRouter;
