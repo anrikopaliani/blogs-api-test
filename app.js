f26cbf06f45cb548aacf4392b6e4bda3f6f32fcd
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-12-31 13:49:56 +0400

    feat:run server

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
+const { MONGODB_URI } = require("./utils/config");
 
-mongoose.connect();
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("connected to mongo"))
+  .catch(() => console.log("unable to connect"));
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
