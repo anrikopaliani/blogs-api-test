516af5f4e2564cb4e85a6f332244ad82026011dd
kopala123 <anri.kopaliani.1@btu.edu.ge>
2024-01-02 14:39:49 +0400

    feat:add references and populate in models

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,25 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    likes: 1,
+    author: 1,
+    id: 1,
+    title: 1,
+  });
 
   response.json(users);
 });
 
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
+
+  if (password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "password length should be more than 3" });
+  }
 
   const saltRound = 10;
   const passwordHash = await bcrypt.hash(password, saltRound);
