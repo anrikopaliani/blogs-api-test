5d625142d27dfeec52a10cde4830682eb6f7b405
kopala123 <anri.kopaliani.1@btu.edu.ge>
2024-01-02 16:27:07 +0400

    feat:token extractor middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -9,12 +9,24 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({ error: error.message });
+  } else if (error.name == "TokenExpiredError") {
+    return response.status(401).json({ error: "token expired" });
   }
 
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  let authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  }
+
+  next();
+};
+
 module.exports = {
   errorHandler,
   unknownEndpoint,
+  tokenExtractor,
 };
